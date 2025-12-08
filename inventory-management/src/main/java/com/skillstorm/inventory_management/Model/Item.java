1cdbd41dcf3cdffaed2694342967e77d59533ba8
Samvabi Lamichhane <lamichhanesamvabi@gmail.com>
2025-12-08 16:19:20 -0600

    Models

--- inventory-management/src/main/java/com/skillstorm/inventory_management/Model/Item.java
+++ inventory-management/src/main/java/com/skillstorm/inventory_management/Model/Item.java
@@ -1,5 +1,158 @@
 package com.skillstorm.inventory_management.Model;
 
-public class Item {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "ITEMS")
+public class Item {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "item_name")
+    private String name;
+
+    @Column 
+    private String sku;
+
+    @Column
+    private int quantity;
+
+    @Column(name = "storage_location")
+    private String storageLocation;
+
+    @ManyToOne
+    @JoinColumn(name = "warehouse_id")
+    private Warehouse warehouse;
+
+    public Item() {
+    }
+
+    public Item(String name, String sku, int quantity, String storageLocation, Warehouse warehouse) {
+        this.name = name;
+        this.sku = sku;
+        this.quantity = quantity;
+        this.storageLocation = storageLocation;
+        this.warehouse = warehouse;
+    }
+
+    public Item(int id, String name, String sku, int quantity, String storageLocation, Warehouse warehouse) {
+        this.id = id;
+        this.name = name;
+        this.sku = sku;
+        this.quantity = quantity;
+        this.storageLocation = storageLocation;
+        this.warehouse = warehouse;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getStorageLocation() {
+        return storageLocation;
+    }
+
+    public void setStorageLocation(String storageLocation) {
+        this.storageLocation = storageLocation;
+    }
+
+    public Warehouse getWarehouse() {
+        return warehouse;
+    }
+
+    public void setWarehouse(Warehouse warehouse) {
+        this.warehouse = warehouse;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((sku == null) ? 0 : sku.hashCode());
+        result = prime * result + quantity;
+        result = prime * result + ((storageLocation == null) ? 0 : storageLocation.hashCode());
+        result = prime * result + ((warehouse == null) ? 0 : warehouse.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Item other = (Item) obj;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (sku == null) {
+            if (other.sku != null)
+                return false;
+        } else if (!sku.equals(other.sku))
+            return false;
+        if (quantity != other.quantity)
+            return false;
+        if (storageLocation == null) {
+            if (other.storageLocation != null)
+                return false;
+        } else if (!storageLocation.equals(other.storageLocation))
+            return false;
+        if (warehouse == null) {
+            if (other.warehouse != null)
+                return false;
+        } else if (!warehouse.equals(other.warehouse))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Item [id=" + id + ", name=" + name + ", sku=" + sku + ", quantity=" + quantity + ", storageLocation="
+                + storageLocation + ", warehouse=" + warehouse + "]";
+    }
 }
--- inventory-management/src/main/java/com/skillstorm/inventory_management/Model/Warehouse.java
+++ inventory-management/src/main/java/com/skillstorm/inventory_management/Model/Warehouse.java
@@ -1,5 +1,144 @@
 package com.skillstorm.inventory_management.Model;
 
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "WAREHOUSES")
 public class Warehouse {
+    
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "warehouse_name")
+    private String name;
+
+    @Column(name = "warehouse_location")
+    private String location;
+
+    @Column(name = "max_capacity")
+    private int max_capacity; 
+
+    @JsonIgnore
+    @OneToMany(targetEntity = Item.class, mappedBy = "warehouse")
+    private Set<Item> items;
+
+    public Warehouse() {
+    }
+
+    public Warehouse(String name, String location, int max_capacity, Set<Item> items) {
+        this.name = name;
+        this.location = location;
+        this.max_capacity = max_capacity;
+        this.items = items;
+    }
+
+    public Warehouse(int id, String name, String location, int max_capacity, Set<Item> items) {
+        this.id = id;
+        this.name = name;
+        this.location = location;
+        this.max_capacity = max_capacity;
+        this.items = items;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getMax_capacity() {
+        return max_capacity;
+    }
+
+    public void setMax_capacity(int max_capacity) {
+        this.max_capacity = max_capacity;
+    }
+
+    public Set<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(Set<Item> items) {
+        this.items = items;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((location == null) ? 0 : location.hashCode());
+        result = prime * result + max_capacity;
+        result = prime * result + ((items == null) ? 0 : items.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Warehouse other = (Warehouse) obj;
+        if (id != other.id)
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (location == null) {
+            if (other.location != null)
+                return false;
+        } else if (!location.equals(other.location))
+            return false;
+        if (max_capacity != other.max_capacity)
+            return false;
+        if (items == null) {
+            if (other.items != null)
+                return false;
+        } else if (!items.equals(other.items))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Warehouse [id=" + id + ", name=" + name + ", location=" + location + ", max_capacity=" + max_capacity
+                + ", items=" + items + "]";
+    }
 
 }
