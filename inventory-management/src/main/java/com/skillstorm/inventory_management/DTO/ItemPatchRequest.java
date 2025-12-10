50c822bef8547d4c92173edd16cd45dc0d5d253a
Samvabi Lamichhane <lamichhanesamvabi@gmail.com>
2025-12-09 23:03:33 -0600

    DTO for Item Patch Requests

---
+++ inventory-management/src/main/java/com/skillstorm/inventory_management/DTO/ItemPatchRequest.java
@@ -0,0 +1,44 @@
+package com.skillstorm.inventory_management.DTO;
+
+public class ItemPatchRequest {
+
+    private String name;
+    private String sku;
+    private Integer quantity;
+    private String storageLocation;
+
+    public ItemPatchRequest() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getStorageLocation() {
+        return storageLocation;
+    }
+
+    public void setStorageLocation(String storageLocation) {
+        this.storageLocation = storageLocation;
+    }
+}
